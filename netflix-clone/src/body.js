32dde666b4cd1806b392bf9eefdb78650d6170e5
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-03-30 10:46:42 -0300

    feat: add header, body and footer - inital skeleton

---
+++ netflix-clone/src/body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default class Body extends React.Component {
+    render() {
+        return (
+            <div>body</div>
+        )
+    }
+}
---
+++ netflix-clone/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <footer id="footer">
+                footer meu filho
+            </footer>
+        )
+    }
+}
