a0ae0027b8c853736642ed8b264010a76a4c085e
Jean Barros <ac.jbarros@gmail.com>
2021-03-31 08:13:02 -0600

    code running

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import './styles/global.scss';
+
+import Header from './landingPage/header.js';
+import Body from './landingPage/body';
+import Footer from './landingPage/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
